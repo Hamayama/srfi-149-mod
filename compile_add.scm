20c40aaa788635395a0ca5594586fa276096c902
Hamayama <hamay1010@gmail.com>
2017-12-12 17:25:05 +0900

    src chg v1.02

--- compile_add.scm
+++ compile_add.scm
@@ -6,17 +6,18 @@
 ;; (overwrite the above definition)
 ;; er-comparer :: (Sym-or-id, Sym-or-id, Env, Env) -> Bool
 (define (er-comparer a b uenv cenv)
-  (or (and (variable? a)
-           (variable? b)
-           ;; recognize both variables and macros
-           (let ([a1 (cenv-lookup-syntax uenv a)]
-                 [b1 (cenv-lookup-syntax uenv b)])
-             (or (eq? a1 b1)
-                 (free-identifier=? a1 b1))))
-      ;; consider keywords
-      (and (not (symbol? a))
-           (not (symbol? b))
-           (keyword? a)
-           (keyword? b)
-           (eq? a b))))
+  (cond
+   ((and (variable? a) (variable? b))
+    ;; recognize both variables and macros
+    (let ([a1 (cenv-lookup-syntax uenv a)]
+          [b1 (cenv-lookup-syntax uenv b)])
+      (or (eq? a1 b1)
+          (free-identifier=? a1 b1))))
+   (else
+    ;; consider keywords
+    (and (not (symbol? a))
+         (not (symbol? b))
+         (keyword? a)
+         (keyword? b)
+         (eq? a b)))))
 
